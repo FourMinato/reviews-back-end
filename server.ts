d9cc2ef2c01c240aebb1d73560917d54ca1eadd1
FourMinato <kropgerken@gmail.com>
2026-03-03 13:29:57 +0700

    port-update

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import http from "http";
 import { app } from "./app";
 import dotenv from 'dotenv';
 dotenv.config();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000; 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log("Server is started");
+  console.log(`Server is started on port ${port}`);
 });
